8725d474d015934276e1866d32082c894a2b79bb
dk1199-code <83574948+dk1199-code@users.noreply.github.com>
2021-05-26 12:24:01 +0530

    Add files via upload

---
+++ VS Code Task-String/task10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task10
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input1;
+            int a=0;
+            char c='a';
+            input1=Console.ReadLine();
+            for(int i=0;i<input1.Length;i++)
+            {
+
+                 char input=input1[i];
+                 int l=0;
+                for(int j=0;j<input1.Length;j++)
+                {
+                    if(input==input1[j])
+                    {
+                        l++;
+                    }
+                }
+                if(l>=a)
+                {
+                    a=l;
+                    c=input;
+                }
+
+            }
+            Console.WriteLine("The most repeted word is :"+c);
+            Console.WriteLine("The number of times word repeated is :"+a);
+
+        }
+    }
+}
---
+++ VS Code Task-String/task11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1;
+            input1=Console.ReadLine();
+            char[] input2=input1.ToCharArray();
+            for(int i=0;i<input1.Length-1;i++)
+            { 
+                for(int j=i+1;j<input1.Length;j++)
+                {
+                if(input2[i]>input2[j])
+                {
+                    char a;
+                    a=input2[i];
+                    input2[i] = input2[j];
+                    input2[j]=a;
+                }
+                }
+            }
+            foreach (char ab in input2)
+            {
+                char c;
+                c=ab;
+                Console.WriteLine(c);                   
+            }
+
+        }
+    }
+}
